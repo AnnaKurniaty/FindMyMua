d61f7cf8e355ebb2060a9cf3ca0c6dc778337f4d
IrfanPertrio <irfanpertrio142002@gmail.com>
2025-08-06 02:05:21 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
 
     public function getProfilePhotoUrlAttribute()
     {
-        if (!$this->muaProfile && !$this->muaProfile->profile_photo) {
+        if (!$this->muaProfile) {
             $defaultAvatar = 'default-avatar.jpeg';
             $supabaseBaseUrl = env('SUPABASE_STORAGE_URL', 'https://fqnrwqaaehzkypgfjdii.supabase.co/storage/v1/object/public/images');
 
