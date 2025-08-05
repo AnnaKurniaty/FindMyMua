ca6a6a9968e3a580db08648714cf3d155e850697
IrfanPertrio <irfanpertrio142002@gmail.com>
2025-08-05 19:56:12 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,11 +5,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Optional: CORS headers (gunakan middleware jika produksi)
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
 // Maintenance mode check
 if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
